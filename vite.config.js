3994155754462ea66edd319aa99709caeab95729
senice222 <sasdasd920@gmail.com>
2025-04-09 14:41:18 +0300

    new update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['api.umiai.ru'] 
+    allowedHosts: ['api.umiai.ru', 'b0fa-188-163-49-245.ngrok-free.app'] 
   }
 })
